e782710d26542929fd40c2f36c5bd3c2d2152355
Adélaïde Sky <augustin.humez@gmail.com>
2023-04-21 23:50:48 +0200

    Compressor ratios

--- Sources/GoXlrKit/Models/DaemonTypesUtilities.swift
+++ Sources/GoXlrKit/Models/DaemonTypesUtilities.swift
@@ -16,44 +16,16 @@ extension Array where Element: (Comparable & SignedNumeric) {
     }
 }
 
-extension Int {
-    public func GetCompRatio() -> CompressorRatio {
-        return CompressorRatio.allCases[self]
-    }
-    public func GetClosestGateTime() -> GateTimes {
-        let listimes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
-        let nearest = listimes.nearest(to: self)!.offset
-        return GateTimes(rawValue: nearest)!
-        
-    }
-    public func GetIntGateTime() -> Int {
-        let listimes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
-        return listimes[self]
-    }
-    
-    public func GetClosestAtkCompTime () -> CompressorAttackTime {
-        let listimes = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 23, 26, 30, 35, 40]
-        let nearest = listimes.nearest(to: self)!.element as Int
-        return CompressorAttackTime(rawValue: "Comp\(nearest)ms")!
-    }
-    public func GetIntCompAtkTime() -> Int {
-        let listimes = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 23, 26, 30, 35, 40]
-        return listimes[self]
-    }
-    public func GetClosestRelCompTime () -> CompressorReleaseTime {
-        let listimes = [0, 15, 25, 35, 45, 55, 65, 75, 85, 95, 100, 115, 140, 170, 230, 340, 680, 1000, 1500, 2000, 3000]
-        let nearest = listimes.nearest(to: self)!.element as Int
-        return CompressorReleaseTime(rawValue: "Comp\(nearest)ms")!
-    }
-    public func GetIntCompRelTime() -> Int {
-        let listimes = [0, 15, 25, 35, 45, 55, 65, 75, 85, 95, 100, 115, 140, 170, 230, 340, 680, 1000, 1500, 2000, 3000]
-        return listimes[self]
-    }
-}
-
 extension GateTimes {
     public var float: Float {
         let listimes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
         return Float(listimes[self.rawValue])
+    }
+}
+
+extension CompressorRatio {
+    public var display: String {
+        let listRatios = ["1:1", "1.1:1", "1.2:1", "1.4:1", "1.6:1", "1.8:1", "2:1", "2.5:1", "3.2:1", "4:1", "5.6:1", "8:1", "16:1", "32:1", "64:1"]
+        return listRatios[self.rawValue]
     }
 }
