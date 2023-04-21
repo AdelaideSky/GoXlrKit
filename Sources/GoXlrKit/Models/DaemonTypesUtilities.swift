357a72ea88004929fcb40c7469d19a157f95318c
Adélaïde Sky <augustin.humez@gmail.com>
2023-04-21 20:19:44 +0200

    updated types RawValue

--- Sources/GoXlrKit/Models/DaemonTypesUtilities.swift
+++ Sources/GoXlrKit/Models/DaemonTypesUtilities.swift
@@ -23,7 +23,7 @@ extension Int {
     public func GetClosestGateTime() -> GateTimes {
         let listimes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
         let nearest = listimes.nearest(to: self)!.element as Int
-        return GateTimes(rawValue: "Gate\(nearest)ms")!
+        return GateTimes(rawValue: nearest)!
         
     }
     public func GetIntGateTime() -> Int {
