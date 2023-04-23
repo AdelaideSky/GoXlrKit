058c2d4155e7f11c552cf866e08e39156a9dfb92
Adélaïde Sky <augustin.humez@gmail.com>
2023-04-23 15:18:43 +0200

    Profiles paths

--- Sources/GoXlrKit/PatchPaths/AddPaths.swift
+++ Sources/GoXlrKit/PatchPaths/AddPaths.swift
@@ -16,6 +16,9 @@ public func handleAddPatch(status: inout StatusClass, path: [String], value: JSO
     if path.contains(["files", "mic_profiles"]) {
         status.files.micProfiles = patch(value: status.files.micProfiles, add: value.stringValue, at: Int(key))
         
+    } else if path.contains(["files", "profiles"]) {
+        status.files.profiles = patch(value: status.files.profiles, add: value.stringValue, at: Int(key))
+        
     } else if path.contains(["files", "presets"]) {
         status.files.presets = patch(value: status.files.presets, add: value.stringValue, at: Int(key))
         
--- Sources/GoXlrKit/PatchPaths/RemovePaths.swift
+++ Sources/GoXlrKit/PatchPaths/RemovePaths.swift
@@ -16,6 +16,9 @@ public func handleRemovePatch(status: inout StatusClass, path: [String], value: 
     if path.contains(["files", "mic_profiles"]) {
         status.files.micProfiles = patch(value: status.files.micProfiles, removeAt: Int(key))
         
+    } else if path.contains(["files", "profiles"]) {
+        status.files.profiles = patch(value: status.files.profiles, removeAt: Int(key))
+        
     } else if path.contains(["files", "presets"]) {
         status.files.presets = patch(value: status.files.presets, removeAt: Int(key))
         
