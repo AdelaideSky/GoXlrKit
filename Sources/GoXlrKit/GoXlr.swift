36f2a8c52a7009e87e619934c353a4d499a36cf6
Adélaïde Sky <augustin.humez@gmail.com>
2023-05-08 20:11:14 +0200

    add import profile support

--- Sources/GoXlrKit/GoXlr.swift
+++ Sources/GoXlrKit/GoXlr.swift
@@ -140,6 +140,31 @@ public class GoXlr: ObservableObject {
         pasteBoard.setString(returnValue, forType: .string)
     }
     
+    public enum profilesubfolders: String {
+        case profiles
+        case micprofiles = "mic-profiles"
+        case presets
+    }
+    
+    public func importProfile(_ url: URL, path: profilesubfolders) {
+        let folderName = "org.GoXLR-on-Linux.GoXLR-Utility/\(path.rawValue)/"
+        let fileManager = FileManager.default
+        if let tDocumentDirectory = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+            let filePath =  tDocumentDirectory.appendingPathComponent("\(folderName)")
+            if !fileManager.fileExists(atPath: filePath.path) {
+                do {
+                    try fileManager.createDirectory(atPath: filePath.path, withIntermediateDirectories: true, attributes: nil)
+                    try fileManager.copyItem(at: url, to: filePath.appendingPathComponent("\(url.lastPathComponent)"))
+                } catch {print("Couldn't create document directory or can't copy file")}
+            }
+            do {
+                try fileManager.copyItem(at: url, to: filePath.appendingPathComponent("\(url.lastPathComponent)"))
+            } catch (let error) {
+                print("error on copying file")
+            }
+        }
+    }
+    
     public init() {}
     
     public enum GoXlrLogLevel {
