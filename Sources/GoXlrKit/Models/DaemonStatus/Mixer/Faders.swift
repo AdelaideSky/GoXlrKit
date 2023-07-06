98f82698169418049b61812928d8fd3b7a5a48c0
Adélaïde Sky <augustin.humez@gmail.com>
2023-07-06 23:32:11 +0200

    Allow setting scribble values

--- Sources/GoXlrKit/Models/DaemonStatus/Mixer/Faders.swift
+++ Sources/GoXlrKit/Models/DaemonStatus/Mixer/Faders.swift
@@ -42,7 +42,7 @@ public class FadersStatus: Codable, ObservableObject {
 public class FaderA: Codable, ObservableObject {
     @Published public var channel: ChannelName { didSet { GoXlr.shared.command(.SetFader(.A, channel)) } }
     @Published public var muteType: MuteFunction { didSet { GoXlr.shared.command(.SetFaderMuteFunction(.A, muteType)) } }
-    @Published public var scribble: Scribble?
+    @Published public var scribble: Scribble? { didSet { handleDidSet(scribble, oldValue)}}
     @Published public var muteState: MuteState
 
     enum CodingKeys: String, CodingKey {
@@ -50,6 +50,19 @@ public class FaderA: Codable, ObservableObject {
         case muteType = "mute_type"
         case scribble
         case muteState = "mute_state"
+    }
+    
+    func handleDidSet(_ value: Scribble?, _ oldValue: Scribble?) {
+        guard value != nil && oldValue != nil else { return }
+        if value?.bottomText != oldValue?.bottomText {
+            GoXlr.shared.command(.SetScribbleText(.A, value!.bottomText))
+        } else if value?.fileName != oldValue?.fileName {
+            GoXlr.shared.command(.SetScribbleIcon(.A, value!.fileName ?? ""))
+        } else if value?.leftText != oldValue?.leftText {
+            GoXlr.shared.command(.SetScribbleNumber(.A, value!.leftText ?? ""))
+        } else {
+            GoXlr.shared.command(.SetScribbleInvert(.A, value!.inverted))
+        }
     }
     
     public required init(from decoder: Decoder) throws {
@@ -72,7 +85,7 @@ public class FaderA: Codable, ObservableObject {
 public class FaderB: Codable, ObservableObject {
     @Published public var channel: ChannelName { didSet { GoXlr.shared.command(.SetFader(.B, channel)) } }
     @Published public var muteType: MuteFunction { didSet { GoXlr.shared.command(.SetFaderMuteFunction(.B, muteType)) } }
-    @Published public var scribble: Scribble?
+    @Published public var scribble: Scribble? { didSet { handleDidSet(scribble, oldValue)}}
     @Published public var muteState: MuteState
 
     enum CodingKeys: String, CodingKey {
@@ -80,6 +93,19 @@ public class FaderB: Codable, ObservableObject {
         case muteType = "mute_type"
         case scribble
         case muteState = "mute_state"
+    }
+    
+    func handleDidSet(_ value: Scribble?, _ oldValue: Scribble?) {
+        guard value != nil && oldValue != nil else { return }
+        if value?.bottomText != oldValue?.bottomText {
+            GoXlr.shared.command(.SetScribbleText(.B, value!.bottomText))
+        } else if value?.fileName != oldValue?.fileName {
+            GoXlr.shared.command(.SetScribbleIcon(.B, value!.fileName ?? ""))
+        } else if value?.leftText != oldValue?.leftText {
+            GoXlr.shared.command(.SetScribbleNumber(.B, value!.leftText ?? ""))
+        } else {
+            GoXlr.shared.command(.SetScribbleInvert(.B, value!.inverted))
+        }
     }
     
     public required init(from decoder: Decoder) throws {
@@ -102,7 +128,7 @@ public class FaderB: Codable, ObservableObject {
 public class FaderC: Codable, ObservableObject {
     @Published public var channel: ChannelName { didSet { GoXlr.shared.command(.SetFader(.C, channel)) } }
     @Published public var muteType: MuteFunction { didSet { GoXlr.shared.command(.SetFaderMuteFunction(.C, muteType)) } }
-    @Published public var scribble: Scribble?
+    @Published public var scribble: Scribble? { didSet { handleDidSet(scribble, oldValue)}}
     @Published public var muteState: MuteState
 
     enum CodingKeys: String, CodingKey {
@@ -110,6 +136,19 @@ public class FaderC: Codable, ObservableObject {
         case muteType = "mute_type"
         case scribble
         case muteState = "mute_state"
+    }
+    
+    func handleDidSet(_ value: Scribble?, _ oldValue: Scribble?) {
+        guard value != nil && oldValue != nil else { return }
+        if value?.bottomText != oldValue?.bottomText {
+            GoXlr.shared.command(.SetScribbleText(.C, value!.bottomText))
+        } else if value?.fileName != oldValue?.fileName {
+            GoXlr.shared.command(.SetScribbleIcon(.C, value!.fileName ?? ""))
+        } else if value?.leftText != oldValue?.leftText {
+            GoXlr.shared.command(.SetScribbleNumber(.C, value!.leftText ?? ""))
+        } else {
+            GoXlr.shared.command(.SetScribbleInvert(.C, value!.inverted))
+        }
     }
     
     public required init(from decoder: Decoder) throws {
@@ -132,8 +171,21 @@ public class FaderC: Codable, ObservableObject {
 public class FaderD: Codable, ObservableObject {
     @Published public var channel: ChannelName { didSet { GoXlr.shared.command(.SetFader(.D, channel)) } }
     @Published public var muteType: MuteFunction { didSet { GoXlr.shared.command(.SetFaderMuteFunction(.D, muteType)) } }
-    @Published public var scribble: Scribble?
+    @Published public var scribble: Scribble? { didSet { handleDidSet(scribble, oldValue)}}
     @Published public var muteState: MuteState
+    
+    func handleDidSet(_ value: Scribble?, _ oldValue: Scribble?) {
+        guard value != nil && oldValue != nil else { return }
+        if value?.bottomText != oldValue?.bottomText {
+            GoXlr.shared.command(.SetScribbleText(.D, value!.bottomText))
+        } else if value?.fileName != oldValue?.fileName {
+            GoXlr.shared.command(.SetScribbleIcon(.D, value!.fileName ?? ""))
+        } else if value?.leftText != oldValue?.leftText {
+            GoXlr.shared.command(.SetScribbleNumber(.D, value!.leftText ?? ""))
+        } else {
+            GoXlr.shared.command(.SetScribbleInvert(.D, value!.inverted))
+        }
+    }
 
     enum CodingKeys: String, CodingKey {
         case channel
