52407ac5f4a11e418cc060b4d31b57a4caada5d5
Adélaïde Sky <augustin.humez@gmail.com>
2023-11-09 16:31:02 +0100

    Make it more useful ofc...

--- Sources/GoXlrKit/GoXLRDynamicValue.swift
+++ Sources/GoXlrKit/GoXLRDynamicValue.swift
@@ -10,30 +10,31 @@ import Combine
 
 @propertyWrapper
 public struct GoXLRDynamicValue<Parent: ObservableObject & GoXLRCommandConvertible, Value: Equatable, Selection: Hashable>: DynamicProperty {
-    private let parentKeyPath: KeyPath<Mixer, Parent>
+    private var parentKeyPath: KeyPath<Mixer, Parent> {
+        paths[selection]!.0
+    }
     private var valueKeyPath: KeyPath<Parent, Value> {
-        paths[selection]!
+        paths[selection]!.1
     }
     private var fullKeyPath: KeyPath<Mixer, Value> {
         parentKeyPath.appending(path: valueKeyPath)
     }
     
-    private let paths: [Selection:KeyPath<Parent, Value>]
+    private let paths: [Selection:(KeyPath<Mixer, Parent>, KeyPath<Parent, Value>)]
     @Binding var selection: Selection
     
     @ObservedObject private var updater = Updater()
     private var cancellable: AnyCancellable? = nil
     
-    public init(_ parentKeyPath: KeyPath<Mixer, Parent>, _ paths: [Selection:KeyPath<Parent, Value>], selection: Binding<Selection>) {
-        self.parentKeyPath = parentKeyPath
+    public init(_ paths: [Selection:(KeyPath<Mixer, Parent>, KeyPath<Parent, Value>)], selection: Binding<Selection>) {
         self._selection = selection
         self.paths = paths
         
         //TODO: MAKE IT SO IT DON'T UPDATE FOR THE WHOLE CLASS
         for path in paths.values {
-            cancellable = GoXlr.shared.mixer![keyPath: parentKeyPath].publisher(for: path).sink { [self] newValue in
+            cancellable = GoXlr.shared.mixer![keyPath: path.0].publisher(for: path.1).sink { [self] newValue in
                 //Maybe check if value has really been updated and not just re-set ?
-                guard path == valueKeyPath else { return }
+                guard path.1 == valueKeyPath else { return }
                 updater.notifyUpdate()
             }
         }
